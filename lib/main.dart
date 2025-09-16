aa3694ebbd3f3a12616393bd4dc7cea4065c098c
Luna Da Silva <117325103+lunadasil@users.noreply.github.com>
2025-09-16 19:07:40 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class InteractiveEmojiApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Interactive Emoji Drawing',
+      title: 'Choose Your Emoji!',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: true,
